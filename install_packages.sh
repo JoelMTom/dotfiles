59b7032a91ef0acd62281e46acc261f71cb4a12d
JoelMTom <joelqwerty007@gmail.com>
2025-02-10 01:32:52 +0530

    Initial Commit

---
+++ install_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+home=$HOME
+
+cd $home
+# echo $(pwd)
+
+### Installing git, base-devel
+
+sudo pacman -S git base-devel --needed
+
+### Installing yay
+
+cd $current_dir
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd $current_dir
+
+
