f16f24cdcb52b6dfb239548ca479eed9765b3d4a
JoelMTom <joelqwerty007@gmail.com>
2025-02-10 18:50:44 +0530

    add utils for yay pacman and initialize function

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+config_dir=$(pwd)
+stow_dir="$HOME/dotfiles"
 
+. "$config_dir/install_packages"
+. "$config_dir/install_dwm"
 
-echo "$HOME"
+initialize() {
+  if [[ ! -d $stow_dir ]]; then
+    echo "Creating stow directory in $HOME"
+    mkdir $stow_dir
+    install_yay()
+  fi
+
+}
+
+initialize
+install_dwm
