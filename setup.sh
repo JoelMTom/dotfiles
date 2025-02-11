c6e160408fbfd578aa92630ec644bffcb688956c
JoelMTom <joelqwerty007@gmail.com>
2025-02-11 19:18:36 +0530

    minor refactoring

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 config_dir=$(pwd)
-stow_dir="$HOME/dotfiles"
+stow_dir="$HOME/dot-config"
+echo "config_dir = $config_dir"
+echo "stow_dir = $stow_dir"
 
 . "$config_dir/install_packages"
 . "$config_dir/install_dwm"
 
 initialize() {
+  echo "Initializing"
   if [[ ! -d $stow_dir ]]; then
     echo "Creating stow directory in $HOME"
     mkdir $stow_dir
     install_yay
   fi
-
 }
 
 initialize
-install_dwm
+install_config_dwm "siduck/chadwm.git"
+
+
