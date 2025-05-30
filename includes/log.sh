5d6a42c81c4185b1307459248b9b8b49dcc4322f
JoelMTom <joelqwerty007@gmail.com>
2025-05-30 14:14:07 +0530

    update logging

--- includes/log.sh
+++ includes/log.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+info_bg=6
+info_fg=0
+success_bg=2
+success_fg=0
+error_bg=1
+error_fg=0
+
+_writeLogColor() {
+    tput setab ${2};tput setaf ${3};echo -n ${1};tput op;tput setaf ${2};
+}
+
 _getLogCategory() {
-  case $1 in
-    0)
-      echo "MESSAGE"
-      ;;
-    1)
-      echo "SUCCESS"
-      ;;
-    2)
-      echo "ERROR"
-      ;;
-    *)
-      echo "UNKNOWN"
-      ;;
-  esac
+    case $1 in
+        0)
+            _writeLogColor "INFO:" ${info_bg} ${info_fg}
+            ;;
+        1)
+            _writeLogColor "SUCCESS:" ${success_bg} ${success_fg}
+            ;;
+        2)
+            _writeLogColor "ERROR:" ${error_bg} ${error_fg}
+            ;;
+        *)
+            echo "UNKNOWN:"
+            ;;
+    esac
 }
 
 _writeLog() {
-  text=$2
-  echo "$(_getLogCategory $1) $text"
+    text=$2
+    echo -n "$(_getLogCategory $1) "
+    echo "$text";tput op
 }
