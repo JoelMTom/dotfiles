75942887f350424d7741a6dbd95cb8463f5cd575
JoelMTom <joelqwerty007@gmail.com>
2026-06-27 17:04:45 +0530

    Update setup.sh file

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@ config_dir=${1:-$(pwd)}
 stow_dir="${config_dir}"
 dot_config_dir="${stow_dir}/dot-config"
 target_dir=${2:-$HOME}
-include_dir="${config_dir}/includes"
 
-source "${include_dir}/log.sh"
-source "${include_dir}/utils.sh"
+source "./includes/log.sh"
+source "./includes/utils.sh"
 
 _writeLog 0 "config_dir = ${config_dir}"
 _writeLog 0 "stow_dir = ${stow_dir}"
@@ -13,19 +12,19 @@ _writeLog 0 "dot_config_dir = ${dot_config_dir}"
 _writeLog 0 "target_dir = ${target_dir}"
 echo
 
-source "${include_dir}/initialize"
-source "${include_dir}/install_packages"
-source "${include_dir}/install_dwm"
-source "${include_dir}/config_nvim"
-source "${include_dir}/config_tmux"
-source "${include_dir}/config_backlight"
-source "${include_dir}/config_dunst"
+source "./includes/initialize"
+source "./includes/install_packages"
+source "./includes/install_dwm"
+source "./includes/config_nvim"
+source "./includes/config_tmux"
+source "./includes/config_backlight"
+source "./includes/config_dunst"
 
 initialize
 required_fonts="ttf-jetbrains-mono noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono-nerd"
 install_packages_using_yay "${required_fonts}" "--needed"
 install_config_dwm "JoelMTom/chadwm.git" "chadwm"
-basic_packages="neovim ghostty exa bat firefox zoxide thefuck fzf plymouth zen-browser-bin cmake yazi light clang llvm nodejs npm unzip dunst batify tmux man bluetui bluez bluez-utils gdb tree-sitter-cli qutebrowser ripgrep sesh"
+basic_packages="neovim ghostty exa bat firefox zoxide thefuck fzf impala plymouth zen-browser-bin cmake yazi light clang llvm nodejs npm unzip dunst batify tmux man bluetui bluez bluez-utils gdb tree-sitter-cli qutebrowser ripgrep sesh xclip maim zsh"
 install_packages_using_yay "${basic_packages}" "--needed"
 config_nvim
 config_backlight
@@ -33,4 +32,4 @@ config_dunst
 config_tmux
 
 install_stow
-stow -d "${stow_dir}" -t "${target_dir}" --dotfiles --ignore='\.*.sh|includes|.git\.*|log' -R .
+stow -d "${stow_dir}" -t "${target_dir}" --dotfiles --ignore='\.*.sh|includes|.git\.*|log|nvim' -R .
